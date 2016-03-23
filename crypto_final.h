9b921fe12666a908006ee8e6f5308e6fc0699026
Mateusz Kulikowski <m.kulikowski@samsung.com>
2016-03-23 14:35:32 +0100

    Remove old code
    Remove code from previous designs (no longer needed)
    
    Change-Id: I3ad0589e3e5559f81053718e44dd797ae3b5d7c7
    Signed-off-by: Mateusz Kulikowski <m.kulikowski@samsung.com>

--- crypto_final.h
+++
@@ -1,8 +0,0 @@
-#ifndef CRYPTO_FINAL_H
-#define CRYPTO_FINAL_H
-
-// TODO: discussion about params
-// A: digest_init etc are helpers,
-// internally they use crypto_init + set param
-///
-#endif // CRYPTO_FINAL_H
